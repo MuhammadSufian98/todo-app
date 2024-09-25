edf2a935b37a6ae65c4113ed6567ee9ad5708ff8
Sufian <sufianhassan98@gmail.com>
2024-09-25 22:14:27 +0500

    Made some more changes to deploy on vercel

--- server/index.js
+++ server/index.js
@@ -6,17 +6,23 @@ const TasksRouter = require("./routers/tasks.js");
 const LoginRouter = require("./routers/login.js");
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
+
 mongoose
-  .connect("mongodb://localhost:27017/ToDoDB")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB", err));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Failed to connect to MongoDB Atlas", err));
 
 app.use(express.json());
-app.use(cors());
-app.use(cors({
-  origin: 'https://todo-3cxau5bpt-muhammad-sufians-projects-e54cc3c2.vercel.app',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin:
+      "https://todo-3cxau5bpt-muhammad-sufians-projects-e54cc3c2.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use("/", TasksRouter);
 app.use("/", LoginRouter);

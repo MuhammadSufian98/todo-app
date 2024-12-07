6e84d23449c0903020c0dbbdf1e25e492b957b3c
Sufian <sufianhassan98@gmail.com>
2024-12-07 12:20:06 +0500

    Add env to mainPage and navBar

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -36,7 +36,7 @@ function NavBar() {
   async function handleTaskSave() {
     const newTask = { Task, ID: fieldRN };
     try {
-      await axios.post(`http://localhost:5000/tasks`, newTask, {
+      await axios.post(`${apiUrl}/tasks`, newTask, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("Token")}`,
         },

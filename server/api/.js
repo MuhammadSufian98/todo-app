fdba991d5f12dffa073098dd786a3162248a57d2
Sufian <sufianhassan98@gmail.com>
2024-12-12 14:10:56 +0500

    More changes to .js file

--- server/api/.js
+++ server/api/.js
@@ -1,8 +1,19 @@
 import { BuiltTime } from "./built-time";
+
 export const Built = (request, response) => {
   response.setHeader('content-type', 'text/plain');
+  
+  const builtDate = new Date(BuiltTime);
+  if (isNaN(builtDate.getTime())) {
+    response.send(`
+      This Serverless Function was built at an unknown time.
+      The current time is ${new Date()}
+    `);
+    return;
+  }
+
   response.send(`
-    This Serverless Function was built at ${new Date(BuiltTime)}.
+    This Serverless Function was built at ${builtDate}.
     The current time is ${new Date()}
   `);
 };

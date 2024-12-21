5563255e84b95c9e3e7f748e96457fef8407d41e
Sufian <sufianhassan98@gmail.com>
2024-12-21 20:16:15 +0500

    Fix bugs

--- server/src/index.js
+++ server/src/index.js
@@ -4,11 +4,43 @@ dotenv.config({path:"./.env"});
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import TasksRouter from "./routers/tasks.js";
-import LoginRouter from "./routers/login.js";
-import ImageRouter from "./routers/ImageUpload.js";
+import TasksRouter from "../routers/tasks.js";
+import LoginRouter from "../routers/login.js";
+import ImageRouter from "../routers/ImageUpload.js";
 import multer from "multer";
-import ImageModel from "./mongoDB/ImageSchema.js"
+
+// import { Configuration, OpenAIApi } from "openai";
+
+// const config = new Configuration({
+// 	apiKey: "YOUR_API_KEY",
+// });
+
+// const openai = new OpenAIApi(config);
+
+// const runPrompt = async () => {
+// 	const prompt = `
+//         write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+
+//         {
+//             "Q": "question",
+//             "A": "answer"
+//         }
+
+//     `;
+
+// 	const response = await openai.createCompletion({
+// 		model: "text-davinci-003",
+// 		prompt: prompt,
+// 		max_tokens: 2048,
+// 		temperature: 1,
+// 	});
+
+// 	const parsableJSONresponse = response.data.choices[0].text;
+// 	const parsedResponse = JSON.parse(parsableJSONresponse);
+
+// 	console.log("Question: ", parsedResponse.Q);
+// 	console.log("Answer: ", parsedResponse.A);
+// };
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
